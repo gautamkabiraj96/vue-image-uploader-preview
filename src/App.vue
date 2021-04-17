314f3a0838d42af01ccd249dab28440d09c436c2
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2021-04-18 01:09:17 +0530

    Extra code removal

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,7 @@ export default {
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
